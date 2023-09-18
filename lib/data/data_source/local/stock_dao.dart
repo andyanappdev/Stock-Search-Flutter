d0860adc77201a476be12bd9651e2ed133484724
Andy An <nationpower81@gmail.com>
2023-09-18 23:32:58 +0900

    <feat>(*): Implement StockDao class

---
+++ lib/data/data_source/local/stock_dao.dart
@@ -0,0 +1,30 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:us_stock/data/data_source/local/corporation_list_entity.dart';
+
+/// Local DB (Hive)에 접근하는 DAO
+class StockDao {
+  /// Hive에 추가
+  Future<void> insertCorporationList(
+      List<CorporationListEntity> corporationListEntities) async {
+    // box open ('stock.db 이름을 가진 Hive Box(DB)를 사용하기 위해 open)
+    final box = await Hive.openBox<CorporationListEntity>('stock.db');
+    await box.addAll(corporationListEntities);
+  }
+
+  /// Hive에 저장된 data Clear
+  Future<void> clearCorporationList() async {
+    final box = await Hive.openBox<CorporationListEntity>('stock.db');
+    await box.clear();
+  }
+
+  /// Hive 내의 data Search
+  Future<List<CorporationListEntity>> searchCorporation(String query) async {
+    final box = await Hive.openBox<CorporationListEntity>('stock.db');
+    final List<CorporationListEntity> corporationList = box.values.toList();
+    return corporationList
+        .where((e) =>
+            e.name.toLowerCase().contains(query.toLowerCase()) ||
+            e.symbol == query.toUpperCase())
+        .toList();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:us_stock/data/data_source/local/corporation_list_entity.dart';
 import 'core/router/router.dart';
 import 'core/color_schemes.dart';
 
@@ -10,6 +11,8 @@ Future<void> main() async {
   await dotenv.load(fileName: '.env');
   // Initializes Hive with a valid directory in your app files.
   await Hive.initFlutter();
+  // Register Adapter
+  Hive.registerAdapter(CorporationListEntityAdapter());
   runApp(const MyApp());
 }
 
