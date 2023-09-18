7add2a88705ce2d2d06f09f6e6a96f428acfe94d
Andy An <nationpower81@gmail.com>
2023-09-18 22:18:30 +0900

    <feat>(*): Implement CorporationListEntity hive adapter class

---
+++ lib/data/data_source/local/corporation_list_entity.dart
@@ -0,0 +1,29 @@
+import 'package:hive/hive.dart';
+
+part 'corporation_list_entity.g.dart';
+
+/// Hive에 저장될 Entity class (객체로 저장할 데이터)
+@HiveType(typeId: 0)
+class CorporationListEntity extends HiveObject {
+  @HiveField(0)
+  String symbol;
+  @HiveField(1)
+  String name;
+  @HiveField(2)
+  String exchange;
+  @HiveField(3)
+  String assetType;
+  @HiveField(4)
+  String ipoDate;
+  @HiveField(5)
+  String status;
+
+  CorporationListEntity({
+    required this.symbol,
+    required this.name,
+    required this.exchange,
+    required this.assetType,
+    required this.ipoDate,
+    required this.status,
+  });
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'core/router/router.dart';
 import 'core/color_schemes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  // Initializes Hive with a valid directory in your app files.
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
