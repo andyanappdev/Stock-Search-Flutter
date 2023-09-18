3a313212d44db7610e02cda07ef6bee168095f46
Andy An <nationpower81@gmail.com>
2023-09-19 01:29:10 +0900

    <feat>(*): Implement StockApi class
    - fetchStocksList method (Listing Status API call)
    - fetchCorporationInfo method (Company Overview API call)

---
+++ lib/data/data_source/remote/stock_api.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class StockApi {
+  static const baseUrl = 'https://www.alphavantage.co';
+  final apiKey = dotenv.get('apiKey');
+  // Client 정의 (test code 작성 및 client 교체를 위해서)
+  final http.Client _client;
+
+  StockApi({http.Client? client}) : _client = (client ?? http.Client());
+
+  /// Listing Status API call
+  Future<http.Response> fetchStocksList() async {
+    return await _client.get(
+        Uri.parse('$baseUrl/query?function=LISTING_STATUS&apikey=$apiKey'));
+  }
+
+  /// Company Overview API call
+  Future<http.Response> fetchCorporationInfo({required String symbol}) async {
+    return await _client.get(Uri.parse(
+        '$baseUrl/query?function=OVERVIEW&symbol=$symbol&apiKey=$apiKey'));
+  }
+}
