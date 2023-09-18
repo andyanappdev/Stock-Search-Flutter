156322258c5579210628c5fd5adeffcc55eb3098
Andy An <nationpower81@gmail.com>
2023-09-18 21:14:45 +0900

    <feat>(*): Implement CorporationList data model class
    - Register assets: .env in pubspec.yaml

---
+++ lib/domain/model/corporation_list.dart
@@ -0,0 +1,21 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'corporation_list.freezed.dart';
+
+part 'corporation_list.g.dart';
+
+@freezed
+class CorporationList with _$CorporationList {
+  const factory CorporationList({
+    required String symbol,
+    required String name,
+    required String exchange,
+    required String assetType,
+    required String ipoDate,
+    required String status,
+  }) = _CorporationList;
+
+  factory CorporationList.fromJson(Map<String, Object?> json) =>
+      _$CorporationListFromJson(json);
+}
