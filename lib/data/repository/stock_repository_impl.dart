27e2d6773a2284649e2a1269e0f32d19a53cd9e5
Andy An <nationpower81@gmail.com>
2023-09-19 21:08:47 +0900

    <refactor>(StockRepositoryImpl): Simplify parser property

--- lib/data/repository/stock_repository_impl.dart
+++ lib/data/repository/stock_repository_impl.dart
@@ -10,9 +10,9 @@ import 'package:us_stock/domain/repository/stock_repository.dart';
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api;
   final StockDao _dao;
-  final CorporationListParser _corporationListParser;
+  final CorporationListParser _parser = CorporationListParser();
 
-  StockRepositoryImpl(this._api, this._dao, this._corporationListParser);
+  StockRepositoryImpl(this._api, this._dao);
 
   @override
   Future<List<CorporationList>> fetchCorporationList(
@@ -31,7 +31,7 @@ class StockRepositoryImpl implements StockRepository {
     // remote에서 가져오기
     try {
       final response = await _api.fetchStocksList();
-      final remoteList = await _corporationListParser.parse(response.body);
+      final remoteList = await _parser.parse(response.body);
       // 기존에 있던 cache 비워주기 (clear 작업이 없으면 계속해서 뒤에 add 되기 때문)
       await _dao.clearCorporationList();
       // 새로 remote 에서 받아온 data cache에 추가
