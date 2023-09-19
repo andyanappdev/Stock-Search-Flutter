c50173aa05d9688f3f2dc85ec005d5441c169a3a
Andy An <nationpower81@gmail.com>
2023-09-19 12:54:56 +0900

    <refactor>(StockRepository): Refactor Return Type
    - Remove result type
    - To be used in usecase

--- lib/domain/repository/stock_repository.dart
+++ lib/domain/repository/stock_repository.dart
@@ -1,10 +1,9 @@
-import 'package:us_stock/core/result.dart';
 import 'package:us_stock/domain/model/corporation_info.dart';
 import 'package:us_stock/domain/model/corporation_list.dart';
 
 abstract interface class StockRepository {
-  Future<Result<List<CorporationList>>> fetchCorporationList(
+  Future<List<CorporationList>> fetchCorporationList(
       bool fetchFromRemote, String query);
 
-  Future<Result<CorporationInfo>> fetchCorporationInfo(String symbol);
+  Future<CorporationInfo> fetchCorporationInfo(String symbol);
 }
