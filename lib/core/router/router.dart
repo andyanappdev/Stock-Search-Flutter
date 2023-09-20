2f2662803c0925e34c8adb57760591f72a85cb9a
Andy An <nationpower81@gmail.com>
2023-09-20 19:02:12 +0900

    <feat>(*): Implement router for MainScreen
    - ChangeNotifierProvider

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,6 +1,12 @@
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:us_stock/data/data_source/local/stock_dao.dart';
+import 'package:us_stock/data/data_source/remote/stock_api.dart';
+import 'package:us_stock/data/repository/stock_repository_impl.dart';
+import 'package:us_stock/domain/use_case/fetch_corporation_list_use_case.dart';
 import 'package:us_stock/presentation/detail_screen/detail_screen.dart';
 import 'package:us_stock/presentation/main/main_screen.dart';
+import 'package:us_stock/presentation/main/main_view_model.dart';
 
 // GoRouter configuration
 final router = GoRouter(
@@ -8,7 +14,13 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/main',
-      builder: (context, state) => MainScreen(),
+      builder: (context, state) {
+        return ChangeNotifierProvider(
+          create: (_) => MainViewModel(FetchCorporationListUseCase(
+              StockRepositoryImpl(StockApi(), StockDao()))),
+          child: const MainScreen(),
+        );
+      },
     ),
     GoRoute(
       path: '/second',
