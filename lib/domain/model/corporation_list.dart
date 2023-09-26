7aec43eae6927516a611150a74a01952f3503b9e
Andy An <nationpower81@gmail.com>
2023-09-26 15:18:17 +0900

    <feat>(CorporationList): Add favorite property
    - To manage favorite corporations

--- lib/domain/model/corporation_list.dart
+++ lib/domain/model/corporation_list.dart
@@ -14,6 +14,7 @@ class CorporationList with _$CorporationList {
     required String assetType,
     required DateTime ipoDate,
     required String status,
+    @Default(false) bool favorite,
   }) = _CorporationList;
 
   factory CorporationList.fromJson(Map<String, Object?> json) =>
