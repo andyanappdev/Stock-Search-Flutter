6c62057b1b640c2df5e9575e551a26f2cfa898aa
Andy An <andyanappdev@gmail.com>
2023-10-04 17:20:36 +0900

    <feat>(router): Add FetchFavoriteCompanyListUseCase

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -4,6 +4,7 @@ import 'package:us_stock/data/data_source/local/stock_dao.dart';
 import 'package:us_stock/data/data_source/remote/stock_api.dart';
 import 'package:us_stock/data/repository/stock_repository_impl.dart';
 import 'package:us_stock/domain/use_case/fetch_corporation_list_use_case.dart';
+import 'package:us_stock/domain/use_case/fetch_favorite_company_list_use_case.dart';
 import 'package:us_stock/presentation/detail_screen/detail_screen.dart';
 import 'package:us_stock/presentation/main/main_screen.dart';
 import 'package:us_stock/presentation/main/main_view_model.dart';
@@ -17,7 +18,7 @@ final router = GoRouter(
       builder: (context, state) {
         return ChangeNotifierProvider(
           create: (_) => MainViewModel(FetchCompanyListUseCase(
-              StockRepositoryImpl(StockApi(), StockDao()))),
+              StockRepositoryImpl(StockApi(), StockDao())), FetchFavoriteCompanyListUseCase(StockRepositoryImpl(StockApi(), StockDao()))),
           child: const MainScreen(),
         );
       },
