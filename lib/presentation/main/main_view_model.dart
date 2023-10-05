c6420b29c0b389206ce8dffea3bba4360229e6e5
Andy An <andyanappdev@gmail.com>
2023-10-06 03:07:11 +0900

    <feat>(MainViewModel): Implement methods related to favoirte and event

--- lib/presentation/main/main_view_model.dart
+++ lib/presentation/main/main_view_model.dart
@@ -1,19 +1,48 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
 import 'package:us_stock/core/result.dart';
-import 'package:us_stock/domain/use_case/fetch_corporation_list_use_case.dart';
+import 'package:us_stock/domain/model/company.dart';
+import 'package:us_stock/domain/use_case/fetch_company_list_use_case.dart';
+import 'package:us_stock/domain/use_case/fetch_favorite_company_list_use_case.dart';
+import 'package:us_stock/domain/use_case/update_favorite_use_case.dart';
+import 'package:us_stock/presentation/main/main_event.dart';
 import 'package:us_stock/presentation/main/main_state.dart';
 
 class MainViewModel with ChangeNotifier {
   final FetchCompanyListUseCase _companyListUseCase;
+  final FetchFavoriteCompanyListUseCase _favoriteCompanyListUseCase;
+  final UpdateFavoriteUseCase _updateFavoriteUseCase;
 
   MainState _state = const MainState();
+
   // getter
   MainState get state => _state;
 
-  MainViewModel(this._companyListUseCase) {
-    _fetchCompanyList();
+  // // debounce용 타이머
+  Timer? _debounce;
+
+  MainViewModel(this._companyListUseCase, this._favoriteCompanyListUseCase, this._updateFavoriteUseCase) {
+    _fetchFavoriteCompanyList();
+  }
+
+  void onEvent(MainEvent event) {
+    switch (event) {
+      case Refresh():
+        // remote에서 데이터 다시 가져오기
+        _fetchCompanyList(fetchFromRemote: true);
+      case SerachQueryChange(:final query):
+        // query가 입력되고 500ms 이후에 실행되도록
+        // (사용자 빠르게 타입하는 것을 모두 반영하지 않고 약간의 딜레이를 주기 위해)
+        _debounce?.cancel();
+        _debounce = Timer(const Duration(microseconds: 300), () {
+          _fetchCompanyList(query: query);
+        });
+      case FavoriteChange():
+        _updateFavorite(event.selectedObject);
+        _fetchCompanyList(fetchFromRemote: false, query: event.query);
+    }
   }
 
   Future<void> _fetchCompanyList(
@@ -33,6 +62,28 @@ class MainViewModel with ChangeNotifier {
 
     // 3. isLoading state 변경
     _state = state.copyWith(isLoading: false);
+    notifyListeners();
+  }
+
+  Future<void> _fetchFavoriteCompanyList() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    final result = await _favoriteCompanyListUseCase.execute();
+    switch (result) {
+      case Success(:final data):
+        _state = state.copyWith(favoriteList: data);
+      case Error(:final message):
+        log('Error MainViewModel: $message');
+    }
+
+    _state = state.copyWith(isLoading: false);
+    notifyListeners();
+  }
+
+  Future<void> _updateFavorite(Company selectedObject) async {
+    await _updateFavoriteUseCase.execute(selectedObject);
+    _fetchFavoriteCompanyList();
     notifyListeners();
   }
 }
