752837b4fc9768d924ed31423920caf15f59e2b9
Andy An <andyanappdev@gmail.com>
2023-10-05 15:30:06 +0900

    <refactor>(MainEvent): Refactor favoriteChange case
    - In order to keep the listview on the screen, take the search term
      from the textfield as a query and process it using

--- lib/presentation/main/main_event.dart
+++ lib/presentation/main/main_event.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:us_stock/domain/model/company.dart';
 
 part 'main_event.freezed.dart';
 
@@ -7,5 +8,5 @@ part 'main_event.freezed.dart';
 sealed class MainEvent with _$MainEvent {
   const factory MainEvent.refresh() = Refresh;
   const factory MainEvent.searchQueryChange(String query) = SerachQueryChange;
-  const factory MainEvent.favoriteChange(int index) = FavoriteChange;
+  const factory MainEvent.favoriteChange(Company selectedObject, String query) = FavoriteChange;
 }
