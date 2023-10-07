10b61b00dd58f79702ab472062f420f868063408
Andy An <andyanappdev@gmail.com>
2023-10-08 03:05:41 +0900

    <feat>(DetailState): Add companyIntradayInfo state

--- lib/presentation/detail_screen/detail_state.dart
+++ lib/presentation/detail_screen/detail_state.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:us_stock/domain/model/company_info.dart';
+import 'package:us_stock/domain/model/company_intraday_info.dart';
 
 part 'detail_state.freezed.dart';
 
@@ -12,6 +13,7 @@ class DetailState with _$DetailState {
     CompanyInfo? companyInfo,
     @Default(false) bool isLoading,
     String? errorMessage,
+    @Default([]) List<CompanyIntradayInfo> companyIntradayInfo,
   }) = _DetailState;
 
   factory DetailState.fromJson(Map<String, Object?> json) => _$DetailStateFromJson(json);
