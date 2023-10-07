3ac61628621099277091ecdb39ac54039bbb15df
Andy An <andyanappdev@gmail.com>
2023-10-08 02:55:28 +0900

    <feat>(StockRepositoryImpl): Implement fetchCompanyIntradayInfo method

--- lib/data/repository/stock_repository_impl.dart
+++ lib/data/repository/stock_repository_impl.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
-import 'package:us_stock/data/csv/corporation_list_parser.dart';
+import 'package:us_stock/data/csv/company_intraday_info_parser.dart';
+import 'package:us_stock/data/csv/company_list_parser.dart';
 import 'package:us_stock/data/data_source/local/company_entity.dart';
 import 'package:us_stock/data/data_source/local/stock_dao.dart';
 import 'package:us_stock/data/data_source/remote/company_info_dto.dart';
@@ -9,12 +10,14 @@ import 'package:us_stock/data/mapper/company_info_mapper.dart';
 import 'package:us_stock/data/mapper/company_mapper.dart';
 import 'package:us_stock/domain/model/company.dart';
 import 'package:us_stock/domain/model/company_info.dart';
+import 'package:us_stock/domain/model/company_intraday_info.dart';
 import 'package:us_stock/domain/repository/stock_repository.dart';
 
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api;
   final StockDao _dao;
-  final CompanyListParser _parser = CompanyListParser();
+  final CompanyListParser _companyListParser = CompanyListParser();
+  final CompanyIntradayInfoParser _companyIntradayInfoParser = CompanyIntradayInfoParser();
 
   StockRepositoryImpl(this._api, this._dao);
 
@@ -34,7 +37,7 @@ class StockRepositoryImpl implements StockRepository {
     // remote에서 가져오기
     try {
       final response = await _api.fetchStocksList();
-      final remoteList = await _parser.parse(response.body);
+      final remoteList = await _companyListParser.parse(response.body);
       // 기존에 있던 cache 비워주기 (clear 작업이 없으면 계속해서 뒤에 add 되기 때문)
       await _dao.clearCompanyList();
       // 새로 remote 에서 받아온 data cache에 추가
@@ -71,6 +74,17 @@ class StockRepositoryImpl implements StockRepository {
       return companyInfoDto.toCompanyInfo();
     } catch (e) {
       throw Exception('Error StockRepositoryImpl fetchCompanyInfo: ${e.toString()}');
+    }
+  }
+
+  @override
+  Future<List<CompanyIntradayInfo>> fetchCompanyIntradayInfo(String symbol) async {
+    try {
+      final response = await _api.fetchCompanyIntradayInfo(symbol: symbol);
+      final result = await _companyIntradayInfoParser.parse(response.body);
+      return result;
+    } catch (e) {
+      throw Exception('Error StockRepositoryImpl fetchCompanyIntradayInfo: ${e.toString()}');
     }
   }
 }
