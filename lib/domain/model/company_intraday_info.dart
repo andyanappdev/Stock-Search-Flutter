91a40fa2876c3882029249e8091ff66ba584ccbd
Andy An <andyanappdev@gmail.com>
2023-10-08 01:05:53 +0900

    <feat>(*): Implement CompanyIntradayInfo data model class

---
+++ lib/domain/model/company_intraday_info.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'company_intraday_info.freezed.dart';
+
+part 'company_intraday_info.g.dart';
+
+@freezed
+class CompanyIntradayInfo with _$CompanyIntradayInfo {
+  const factory CompanyIntradayInfo({
+    required DateTime timeStamp,
+    required double open,
+    required double high,
+    required double low,
+    required double close,
+    required double volume,
+  }) = _CompanyIntradayInfo;
+
+  factory CompanyIntradayInfo.fromJson(Map<String, Object?> json) => _$CompanyIntradayInfoFromJson(json);
+}
