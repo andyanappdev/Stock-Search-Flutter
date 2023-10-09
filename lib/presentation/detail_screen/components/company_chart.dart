ae3d8d47083b7ad61744756aad9615cab5d1bc04
Andy An <andyanappdev@gmail.com>
2023-10-10 04:06:06 +0900

    <feat>(CompanyChart): Implement draw FastLine chart

--- lib/presentation/detail_screen/components/company_chart.dart
+++ lib/presentation/detail_screen/components/company_chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:us_stock/domain/model/company_intraday_info.dart';
@@ -12,22 +13,53 @@ class CompanyChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<DetailViewModel>();
-    final List<CompanyIntradayInfo> chartData = viewModel.state.companyIntradayInfo;
+    // List<ChartData> chartData = getChartData();
+    final List<CompanyIntradayInfo> chartData =
+        viewModel.state.companyIntradayInfo;
     return SizedBox(
       width: double.infinity,
-      height: 300,
-      child: SfCartesianChart(
-        // // title: ChartTitle(text: 'Intraday Chart'),
-        // primaryXAxis: DateTimeAxis(),
-        // primaryYAxis: NumericAxis(),
-        // series: <ChartSeries>[
-        //   // Renders fast line chart
-        //   FastLineSeries<CompanyIntradayInfo, DateTime>(
-        //     dataSource: chartData,
-        //     xValueMapper: (CompanyIntradayInfo data, _) => data.timeStamp,
-        //     yValueMapper: (CompanyIntradayInfo data, _) => data.close,
-        //   )
-        // ],
+      // height: 600,
+      child: Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 250,
+            child: SfCartesianChart(
+              // title: ChartTitle(text: 'Intraday Chart'),
+              primaryXAxis: DateTimeAxis(interval: 1),
+              primaryYAxis: NumericAxis(
+                numberFormat: NumberFormat.simpleCurrency(decimalDigits: 0),
+                opposedPosition: true,
+                labelPosition: ChartDataLabelPosition.outside,
+              ),
+              plotAreaBorderWidth: 0,  // 그래프 외곽선
+              series: <ChartSeries>[
+                // Renders fast line chart
+                FastLineSeries<CompanyIntradayInfo, DateTime>(
+                  dataSource: chartData,
+                  xValueMapper: (CompanyIntradayInfo data, _) => data.timeStamp,
+                  yValueMapper: (CompanyIntradayInfo data, _) => data.close,
+                  color: Colors.blueAccent,
+                ),
+              ],
+            ),
+          ),
+          // SizedBox(
+          //   width: double.infinity,
+          //   height: 50,
+          //   child: SfCartesianChart(
+          //     primaryYAxis: DateTimeCategoryAxis(interval: 1),
+          //     primaryXAxis: NumericAxis(),
+          //     series: <ChartSeries>[
+          //       ColumnSeries<CompanyIntradayInfo, DateTime>(
+          //         dataSource: chartData,
+          //         xValueMapper: (CompanyIntradayInfo data, _) => data.timeStamp,
+          //         yValueMapper: (CompanyIntradayInfo data, _) => data.volume,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+        ],
       ),
     );
   }
