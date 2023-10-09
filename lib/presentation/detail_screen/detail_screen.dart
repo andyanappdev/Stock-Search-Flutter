4cb577a386ee07755b90c1d1089d7df436485b21
Andy An <andyanappdev@gmail.com>
2023-10-10 04:10:32 +0900

    <feat>(DetailScreen): Implement most of default UI

--- lib/presentation/detail_screen/detail_screen.dart
+++ lib/presentation/detail_screen/detail_screen.dart
@@ -24,7 +24,9 @@ class DetailScreen extends StatelessWidget {
                 radius: 20.0,
                 color: CupertinoColors.link,
               )),
-            if (state.isLoading == false && state.companyInfo != null && state.companyIntradayInfo.isNotEmpty)
+            if (state.isLoading == false &&
+                state.companyInfo != null &&
+                state.companyIntradayInfo.isNotEmpty)
               _buildBody(context, viewModel, state),
           ],
         ),
@@ -91,22 +93,42 @@ class DetailScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildBody(BuildContext context, DetailViewModel viewModel, DetailState state) {
+  Widget _buildBody(
+      BuildContext context, DetailViewModel viewModel, DetailState state) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                  '${state.companyInfo!.exchange} - ${state.companyInfo!.currency}'),
-            ],
-          ),
-          const Divider(),
-          if (state.companyIntradayInfo.isNotEmpty) _buildCompanyChart(context, viewModel),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  state.companyIntradayInfo.last.close.toString(),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                Text(
+                    '${state.companyInfo!.exchange} - ${state.companyInfo!.currency}'),
+              ],
+            ),
+            const Divider(),
+            if (state.companyIntradayInfo.isNotEmpty)
+              _buildCompanyChart(context, viewModel),
+            const SizedBox(height: 10),
+            _buildCompanyMetrics(context, state, viewModel),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Divider(),
+            ),
+            _buildBasicInfo(context, state),
+          ],
+        ),
       ),
     );
   }
@@ -115,4 +137,309 @@ class DetailScreen extends StatelessWidget {
     return CompanyChart(viewModel: viewModel);
   }
 
+  Widget _buildCompanyMetrics(
+      BuildContext context, DetailState state, DetailViewModel viewModel) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '개장가',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                '최고가',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                '최저가',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+            ],
+          ),
+          const SizedBox(width: 18.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                state.companyIntradayInfo.first.open.toStringAsFixed(2),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                viewModel.calculateValue('max').toStringAsFixed(2),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                viewModel.calculateValue('min').toStringAsFixed(2),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+          const SizedBox(width: 50.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '시가총액',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                'PER',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                'EPS',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+            ],
+          ),
+          const SizedBox(width: 18.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                viewModel
+                    .formatMarketCap(state.companyInfo!.marketCapitalization),
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.per,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.eps,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+          const SizedBox(width: 50.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '배당 수익률',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                '이익 마진',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                'PBR',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+            ],
+          ),
+          const SizedBox(width: 18.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                state.companyInfo!.dividendYield,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.profitMargin,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.priceToBookRatio,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+          const SizedBox(width: 50.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '52주 최고',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                '52주 최저',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+              Text(
+                '베타',
+                style: TextStyle(
+                    fontSize: 16, color: Theme.of(context).colorScheme.outline),
+              ),
+            ],
+          ),
+          const SizedBox(width: 18.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                state.companyInfo!.weekHigh,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.weekLow,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+              Text(
+                state.companyInfo!.beta,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBasicInfo(BuildContext context, DetailState state) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Basic Information',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w900,
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text.rich(
+            TextSpan(
+              text: 'Country : ', // default text style
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.outline),
+              children: [
+                TextSpan(
+                  text: state.companyInfo!.country,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text.rich(
+            overflow: TextOverflow.ellipsis,
+            TextSpan(
+              text: 'Address : ', // default text style
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.outline),
+              children: [
+                TextSpan(
+                  text: state.companyInfo!.address,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text.rich(
+            overflow: TextOverflow.ellipsis,
+            TextSpan(
+              text: 'Sector : ', // default text style
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.outline),
+              children: [
+                TextSpan(
+                  text: state.companyInfo!.sector,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text.rich(
+            overflow: TextOverflow.ellipsis,
+            TextSpan(
+              text: 'Industry : ', // default text style
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Theme.of(context).colorScheme.outline),
+              children: [
+                TextSpan(
+                  text: state.companyInfo!.industry,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Theme.of(context).colorScheme.onBackground),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text(
+            'Description',
+            style: TextStyle(
+                fontSize: 15,
+                color: Theme.of(context).colorScheme.outline),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2.0),
+          child: Text(
+            state.companyInfo!.description,
+            style: TextStyle(
+                fontSize: 15,
+                color: Theme.of(context).colorScheme.onBackground),
+            // overflow: TextOverflow.ellipsis,
+            // maxLines: null,
+          ),
+        ),
+      ],
+    );
+  }
 }
